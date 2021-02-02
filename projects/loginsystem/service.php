40fcc3b28bca9b0c0afb67af018b4b3f32dab74f
kelvo1999 <kelvoshisanya@gmail.com>
2021-02-02 14:23:09 +0300

    added projects

---
+++ projects/loginsystem/service.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>our service</title>
+    <link rel="stylesheet" href="service.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="services">
+        <h1><span>Our</span> Services</h1>
+        <div class="content">
+            <div class="service">
+                <i class="fa fa-case"></i>
+                <h2>Case Reporting</h2>
+                <p> A victim reports a case in a written document stating what happenned in detail. Each case report is a legal document that becomes a permanent written record of that event or incident.</p>
+            </div>
+            <div class="service">
+                <i class="fa fa-report"></i>
+                <h2>Investigative report</h2>
+                <p>An investigative report is a written document prepared by a peace officer that records in detail the officer's observations and actions as they relate to a specific event or incident. Each investigative report is a legal document that
+                    becomes a permanent written record of that event or incident.</p>
+            </div>
+            <div class="service">
+                <i class="fa fa-police"></i>
+                <h2>Police Records</h2>
+                <p>Police and other law enforcement agencies investigating crimes compile a number of different records in criminal cases. These include:
+                    <ul>
+                        <li>arrest and incident reports (which are usually difficult to obtain)</li>
+                        <li>logs of arrests and responses to incidents by police (which usually are open to the public)</li>
+                        <li>search warrants and coroner’s office reports (which also usually are open to public inspection)</li>
+                    </ul>
+                </p>
+            </div>
+            <div class="service">
+                <i class="fa fa-arrest"></i>
+                <h2>Arrest Report</h2>
+                <p>This report provides details on an arrest made by police.</p>
+            </div>
+            <div class="service">
+                <i class="fa fa-help"></i>
+                <h2>24/7 Helpline</h2>
+                <p>It is a telephone service which offers help to those who call.</p>
+            </div>
+            <div class="service">
+                <i class="fa fa-emergency"></i>
+                <h2>Emergency Call</h2>
+                <p>For any emergency, call <b>911</b></p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
