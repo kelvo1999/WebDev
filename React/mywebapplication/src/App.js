5bae09c3ba060cb607f59da01d023fecc52191cf
[kelvo1999] <[kelvoshisanya@gmail.com]>
2021-03-10 17:18:49 +0300

    all

--- React/mywebapplication/src/App.js
+++ React/mywebapplication/src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Students from "./Students";
 import Library from "./Library";
 function App() {
   return (
     <div>
-      
+      <Router>
 
-      <Students />
-      <Library />
+      <Route exact path="/">
+      <img src={{}}>
+      </Router>
+      
     </div>
       
   );
